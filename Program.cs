b17e8ca4cc369c5196bc7277240c5706136083b8
Cintia Abe <cintia.rossetto@gmail.com>
2023-01-02 20:00:38 -0300

    Finalizando Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone CelularCintia = new Nokia("19987896325","NK45", "45699878544785im", 256 );
+CelularCintia.Ligar();
+Smartphone CelularAbe = new Iphone("19789632558", "14", "ip1458796587", 128);
+CelularAbe.ReceberLigacao();
+CelularAbe.InstalarAplicativo("Whatsapp");
